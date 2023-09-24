1715bf3501ea6fc8621a85b972e8353a416e008e
Jorge Armando <jorgejc25@gmail.com>
2023-09-24 00:04:59 -0500

    Update axios-config.js

--- src/helpers/axios-config.js
+++ src/helpers/axios-config.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
     // baseURL: 'http://localhost:5000/api/'
-    baseURL: 'http://ecommerce.appenlaweb.com:5000/api/'
+    // baseURL: 'http://ecommerce.appenlaweb.com:5000/api/'
+    baseURL: 'https://global-store-bk.onrender.com/api/'
 });
 
 export {
