870f63361a76a7d35597de0b41d9c92ec6c04ea5
jorgejciud <jorge.julio@iudigital.edu.co>
2023-09-23 20:23:28 -0500

    código inicial

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Header } from './components/ui/Header';
+import { Carousel } from './components/ui/Carousel';
+import { BrowserRouter as Router, Switch, Route, Redirect, useLocation } from "react-router-dom";
+import { OrdersView } from './components/orders/OrdersView';
+import { ProfileView } from './components/profile/ProfileView';
+import { ProductView } from './components/products/ProductView';
+import { ProductNew } from './components/products/ProductNew';
+import { Login } from './components/auth/Login';
+import { Logout } from './components/auth/Logout';
+import { Register } from './components/auth/Register';
+import PrivateRoute from './components/shared/PrivateRoute';
+import PublicRoute from './components/shared/PublicRoute';
+import AuthContextProvider from './contexts/authContext';
+
+
+const App = () => {
+
+    const location = useLocation();
+
+    return (
+        <AuthContextProvider>
+            <Router>
+            <Header />
+            <Carousel />
+                <Switch>
+                    <Route exact path='/' component={ProductView} />
+                    <Route exact path='/productos' component={ProductView} />
+                    {/* <Route exact path='/' component={ProductNew} /> */}
+                    {/* <PrivateRoute path='/productos' component={ProductNew} />
+                    <PrivateRoute path='/logout' component={Logout} />
+                    <Route exact path='/orders' component={OrdersView} />
+                    <Route exact path='/profile' component={ProfileView} />
+                    <PublicRoute exact path="/login" component={Login} />
+                    <Route exact path="/register" component={Register} /> */}
+                    <Redirect to='/' />
+                </Switch>
+            </Router>
+        </AuthContextProvider>
+    );
+}
+
+export {
+    App
+}
